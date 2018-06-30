824a38665a0a5600accd9f92f220ee0649e3047e
shienyi <404621962@qq.com>
2018-06-30 14:12:50 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.route('/login')
+.get(function(req, res) {
+  res.render('login', { title: '用户登录' });
+})
+.post(function(req, res) {
+  var user={
+    username: 'shienyi',
+    password: '123456'
+  }
+  if(req.body.username === user.username && req.body.password === user.password){
+    res.redirect('/home');
+  }
+  res.redirect('/login');
+});
+ 
+router.get('/logout', function(req, res) {
+  res.redirect('/');
+});
+ 
+router.get('/home', function(req, res) {
+  var user={
+    username:'shienyi',
+    password:'123456'
+  }
+  res.render('home', { title: 'Home', user: user });
+});
+
+module.exports = router;
+
